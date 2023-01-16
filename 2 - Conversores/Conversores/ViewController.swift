8f97fd0dd71097102c719483700597cb89daefa5
Bruno Rafael <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-01-16 15:06:45 -0300

    Inicio do segundo projeto
    Upando backup do segundo projeto

---
+++ 2 - Conversores/Conversores/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Bruno on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfvalue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UITextField!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
+
+
---
+++ MyPlaygroundBlank.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name = "Bruno Silva"
+var number = 7 + 2
+
+print("a soma de 7+2 é igual a",number)
+
+var a = 0
+for i in 1...15 {
+    a = a + i
+}
+
+print(a)
