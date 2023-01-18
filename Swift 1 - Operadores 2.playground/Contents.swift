1936bdd4609b998f01f9c48cf8c945102fd857b6
Bruno Rafael <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-01-18 14:52:35 -0300

    ATT

--- Swift 1 - Operadores 2.playground/Contents.swift
+++ Swift 1 - Operadores 2.playground/Contents.swift
@@ -10,3 +10,29 @@ var boughtTomato: Bool = true
 var boughtApple = false
 
 var isHappy = boughtBanana && boughtTomato && boughtApple
+
+true && true
+true && false
+false && false
+false && true
+
+// Operador OU(OR): ||
+var likesMeat = false
+var likesBeer = true
+var canInviteBarbecue = likesBeer || likesMeat
+//retorna true se ao menos uma das alternativas forem verdadeiras
+
+var grade1 = 8.5
+var grade2 = 7.0
+let result = (grade1+grade2)/2
+
+//Operador de negação (NOT) !
+var knowSwift = false
+var enrolSwiftCourse = !knowSwift
+
+//Operador ternário
+var grade = 7.95
+var graderesult = (grade >= 7.0) ? "Aprovado" : "Reprovado"
+
+let number = 11
+var type = (number % 2 == 0) ? "Par" : "Ímpar"
